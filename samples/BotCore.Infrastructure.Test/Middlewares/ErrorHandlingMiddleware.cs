c224200e0349e0777a41842842e236711cb5380e
Dmitriy <dmitriy.vorobey1@gmail.com>
2020-09-13 19:50:47 +0300

    added ErrorHandlingMiddleware.cs

--- samples/BotCore.Infrastructure.Test/Middlewares/ErrorHandlingMiddleware.cs
+++ samples/BotCore.Infrastructure.Test/Middlewares/ErrorHandlingMiddleware.cs
@@ -26,6 +26,7 @@ namespace BotCore.Infrastructure.Test.Middlewares
             catch (Exception ex)
             {
                 _logger.LogError(ex, ex.Message);
+                Console.WriteLine(ex);
                 throw;
             }
         }
