497c8a3c32b0a91c0267bf7215843869acf71caa
Dmitriy <dmitriy.vorobey1@gmail.com>
2020-09-20 00:00:53 +0300

    added base test project for core

---
+++ tests/BotCore.Telegram.Tests/DependencyInjection/IModule.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BotCore.Telegram.Tests.DependencyInjection
+{
+    public interface IModule
+    {
+        void Load(IServiceCollection collection, IConfiguration configuration);
+    }
+}
