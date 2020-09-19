c750c3683f0042c08e94420355b9e20a2c30784b
Dmitriy <dmitriy.vorobey1@gmail.com>
2020-09-19 03:13:53 +0300

    renamed projects and added test infrastructure

---
+++ samples/BotCore.CurrencyBot.API/Controllers/MainController.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using BotCore.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot.Types;
+using Viber.Bot;
+
+namespace BotCore.CurrencyBot.API.Controllers
+{
+    [ApiController]
+    [Route("bot")]
+    public class MainController : Controller
+    {
+        private readonly IHandler<Update> _telegramHandler;
+        private readonly IHandler<CallbackData> _viberHandler;
+
+        public MainController(IHandler<Update> telegramHandler, IHandler<CallbackData> viberHandler)
+        {
+            _telegramHandler = telegramHandler;
+            _viberHandler = viberHandler;
+        }
+
+        [HttpPost("telegram")]
+        public async Task<IActionResult> Update([FromBody] Update telegramUpdate)
+        {
+            await _telegramHandler.HandleUpdate(telegramUpdate);
+            return Ok();
+        }
+
+        [HttpPost("viber")]
+        public async Task<IActionResult> Update([FromBody] CallbackData viberUpdate)
+        {
+            await _viberHandler.HandleUpdate(viberUpdate);
+            return Ok();
+        }
+    }
+}
---
+++ samples/BotCore.CurrencyBot.API/Startup.cs
@@ -0,0 +1,86 @@
+using System;
+using BotCore.Core.CurrencyBot.Interfaces;
+using BotCore.Core.Interfaces;
+using BotCore.CurrencyBot.Infrastructure.Data;
+using BotCore.CurrencyBot.Infrastructure.Middlewares;
+using BotCore.CurrencyBot.Infrastructure.Services;
+using BotCore.Telegram;
+using BotCore.Telegram.CurrencyBot.Actions;
+using BotCore.Telegram.DomainModels;
+using BotCore.Viber;
+using BotCore.Viber.DomainModels;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BotCore.CurrencyBot.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IWebHostEnvironment currentEnvironment)
+        {
+            Configuration = configuration;
+            CurrentEnvironment = currentEnvironment;
+        }
+
+        public IConfiguration Configuration { get; }
+        private IWebHostEnvironment CurrentEnvironment { get; }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var isProd = CurrentEnvironment.IsProduction();
+            services.AddControllers().AddNewtonsoftJson();
+
+            services.AddTelegramClient(Configuration, isProd);
+            services.AddViberClient(Configuration, isProd);
+
+            services.AddScoped<IAction<TelegramCommand>, GetAllCurrenciesAction>();
+            services.AddScoped<IAction<TelegramCommand>, GetConcreteCurrencyRateAction>();
+            services.AddScoped<IAction<TelegramCommand>, GetCurrencyRateAction>();
+            services.AddScoped<IAction<TelegramCommand>, SetupDefaultCurrenciesAction>();
+            services.AddScoped<IAction<TelegramCommand>, StartAction>();
+
+
+            services.AddScoped<IAction<ViberCommand>, Viber.CurrencyBot.Actions.GetAllCurrenciesAction>();
+            services.AddScoped<IAction<ViberCommand>, Viber.CurrencyBot.Actions.GetCurrencyRateAction>();
+            services.AddScoped<IAction<ViberCommand>, Viber.CurrencyBot.Actions.StartAction>();
+
+            services.AddScoped<IApiProvider, ApiProvider>();
+            services.AddScoped<ICurrencyService, CurrencyService>();
+            services.AddScoped<IDbCacheService, DbCacheService>();
+            services.AddScoped<IMessageService, MessageService>();
+            services.AddScoped<IUsersService, UsersService>();
+
+            if (isProd)
+                services.AddDbContext<BotCoreTestContext>(options =>
+                    options.UseNpgsql(Environment.GetEnvironmentVariable("BotCoreDbConnectionString")));
+            else
+                services.AddDbContext<BotCoreTestContext>(options =>
+                    options.UseNpgsql(Configuration.GetConnectionString("BotCoreTestContext")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(GenericRepository<>));
+
+            services.AddTelegramActionsExecutor();
+            services.AddViberActionsExecutor();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            app.UseMiddleware<ErrorHandlingMiddleware>();
+
+            app.UseRouting();
+            app.UseCors();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
