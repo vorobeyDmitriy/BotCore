db74e5c003f857ebaa1b5cc2e0ed5f91534bf161
Dmitriy <dmitriy.vorobey1@gmail.com>
2020-09-20 17:19:29 +0300

    added tests for TelegramMessageSender.cs

---
+++ tests/BotCore.Telegram.Tests/Tests/TelegramMessageSenderTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using BotCore.Core;
+using BotCore.Core.Interfaces;
+using BotCore.Telegram.DomainModels;
+using BotCore.Telegram.Tests.Modules;
+using BotCore.Tests;
+using BotCore.Tests.DependencyInjection;
+using NUnit.Framework;
+
+namespace BotCore.Telegram.Tests.Tests
+{
+    public class TelegramMessageSenderTests : TestClassBase
+    {
+        protected override IModule Module => new TelegramMessageSenderTestModule();
+
+        [Test]
+        public void SendTextAsync_NullMessage_ThrowsArgumentNullException()
+        {
+            var messageSender = GetService<IMessageSender<TelegramMessage>>();
+
+            var message = (TelegramMessage) null;
+
+            Assert.ThrowsAsync<ArgumentNullException>(async () => await messageSender.SendTextAsync(message));
+        }
+
+        [Test]
+        public async Task SendTextAsync_UnsuccessfullySend_UnsuccessfullyResult()
+        {
+            var messageSender = GetService<IMessageSender<TelegramMessage>>();
+
+            var message = new TelegramMessage
+            {
+                Receiver = "-1"
+            };
+
+            var result = await messageSender.SendTextAsync(message);
+            Assert.False(result.Success);
+            Assert.AreEqual(Constants.Error, result.Error);
+        }
+
+        [Test]
+        public async Task SendTextAsync_SuccessfullySend_SuccessfullyResult()
+        {
+            var messageSender = GetService<IMessageSender<TelegramMessage>>();
+
+            var message = new TelegramMessage
+            {
+                Receiver = "123"
+            };
+
+            var result = await messageSender.SendTextAsync(message);
+            Assert.True(result.Success);
+            Assert.AreEqual(string.Empty, result.Error);
+        }
+    }
+}
