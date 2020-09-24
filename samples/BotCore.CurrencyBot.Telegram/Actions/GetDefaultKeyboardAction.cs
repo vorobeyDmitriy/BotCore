6089c08a695dd28013b67041c53e93e5f74b3583
Dmitriy <dmitriy.vorobey1@gmail.com>
2020-09-24 22:31:53 +0300

    get default bot keyboard

---
+++ samples/BotCore.CurrencyBot.Telegram/Actions/GetDefaultKeyboardAction.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using BotCore.Core.CurrencyBot.Constants;
+using BotCore.Core.DomainModels;
+using BotCore.Core.Interfaces;
+using BotCore.Telegram.CurrencyBot.Keyboards;
+using BotCore.Telegram.DataTransfer;
+using BotCore.Telegram.DomainModels;
+
+namespace BotCore.Telegram.CurrencyBot.Actions
+{
+    public class GetDefaultKeyboardAction: TelegramAction
+    {
+        public GetDefaultKeyboardAction(IMessageSender<TelegramMessage> messageSender) : base(messageSender)
+        {
+        }
+
+        public override async Task<OperationResult> ExecuteAsync(TelegramCommand commandBase)
+        {
+            return await MessageSender.SendTextAsync(new TelegramMessage
+            {
+                Receiver = commandBase.ChatId.ToString(),
+                Keyboard = GetCurrencyRateKeyboard.Keyboard,
+                Text = MessagesConstants.Hello
+            });
+        }
+    }
+}
